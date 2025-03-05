5fe5ae084086458893dfe4f1c69f61cf09f53e5f
Tiana Nguyen Vo <tianaovo@Tianas-Mac.local>
2025-03-05 13:33:30 -0500

    cleaned up basic setup

--- gpio_setup.py
+++ gpio_setup.py
@@ -27,8 +27,6 @@ import time
 
 #GPIO.setmode(GPIO.BCM)
 
-# connect ENA 1 and 2 to 5V power on Raspberry Pi?
-
 # NEMA23 Stepper Motor (x-axis)
 #ENAX = 17  #gpio
 DIRX = 27  
@@ -39,52 +37,23 @@ PULX = 22
 DIRY = 24  
 PULY = 25 
 
-# set all pins to output mode
-#GPIO.setup(ENA1, GPIO.OUT)
-#GPIO.setup(DIRX, GPIO.OUT)
-#GPIO.setup(PULX, GPIO.OUT)
-#GPIO.setup(ENA2, GPIO.OUT)
-#GPIO.setup(DIRY, GPIO.OUT)
-#GPIO.setup(PULY, GPIO.OUT)
-
-#h = lgpio.gpiochip_open(0)
-
-#lgpio.gpio_claim_output(h, DIRX)
-#lgpio.gpio_claim_output(h, DIRY)
-#lgpio.gpio_claim_output(h, PULX)
-#lgpio.gpio_claim_output(h, PULY)
 dirX = LED(DIRX)
 dirY = LED(DIRY)
 pulX = LED(PULX)
 pulY = LED(PULY)
 
 #def move_stepper(ena, dir_pin, pul_pin, steps, delay=0.001):
-def move_stepper(dir_pin, pul_pin, steps, delay=0.002):
+def move_stepper(dir_pin, pul_pin, steps, delay=0.0015):
 
-    #GPIO.output(ena, GPIO.HIGH)         # enable motor
-    #GPIO.output(dir_pin, GPIO.HIGH)     # set direction
-    #lgpio.gpio_write(h, dir_pin, 1)
-    dirX.on()
-
-    # generate pulses for the specified number of steps
+    dir_pin.on()
+    
     for _ in range(steps):
-        #GPIO.output(pul_pin, GPIO.HIGH)
-        #lgpio.gpio_write(h, pul_pin, 1)
-        pulX.on()
+        pul_pin.on()
         time.sleep(delay)
-        #GPIO.output(pul_pin, GPIO.LOW)
-        #lgpio.gpio_write(h, pul_pin, 0)
-        pulX.off()
+        pul_pin.off()
         time.sleep(delay)
+    
+    dir_pin.off()
 
-    #GPIO.output(ena, GPIO.LOW)          # disable the motor after movement
-
-move_stepper(DIRX, PULX, 1000)    # 1000 steps
-move_stepper(DIRY, PULY, 500)     # 500 steps
-
-#GPIO.cleanup()                          # clean up gpio configuration
-#lgpio.gpio_free(h, DIRX)
-#lgpio.gpio_free(h, DIRY)
-#lgpio.gpio_free(h, PULX)
-#lgpio.gpio_free(h, PULY)
-#lgpio.gpiochip_close(h)
+move_stepper(dirX, pulX, 1000)      #dirX = left, dirY = right, pulX = horizontanl (x) movement,    1000 = # steps)
+move_stepper(dirX, pulY, 500)       #dirX = up,   dirY = down,    pulY = vertical (y) movement,        500 = # steps)
